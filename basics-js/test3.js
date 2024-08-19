f757b8f864dacc4c45b095a263135cb3459f4682
Muhammad Abdullah <ma.abdullahspan@gmail.com>
2024-08-19 13:50:44 +0000

    datatypes

---
+++ basics-js/test3.js
@@ -0,0 +1,27 @@
+"use strict"; //treats all code as newer version
+
+// alert(3 + 3) //we are using node.js
+
+
+// console.log(Abdullah)
+
+
+// let name = "abdullah"]
+// let age = 19
+// let isLoggedIn = false
+
+//premative data types
+//number => 2 power 53
+//bigint
+//string =>""
+//boolean => true/falsu
+//null => stand alone value 
+//undefined =>
+//symbol => unique
+
+//object
+
+
+console.log(typeof undefined);// type undefined
+console.log(typeof null);// type object
+
