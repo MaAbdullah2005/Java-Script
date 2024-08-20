e43061b18dab834d6d82f98b6b491b01d3435a14
Muhammad Abdullah <ma.abdullahspan@gmail.com>
2024-08-20 12:14:30 +0000

    conversion

---
+++ basics-js/Datatype-conversion.js
@@ -0,0 +1,60 @@
+//let score = "abdullah"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+//true => 1; false => 0
+
+
+//let isLoggedIn = "abudllah"
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=> false
+// "" => false
+// "abdullah" => true
+
+//let someNumber = 33
+
+//let stringNumber = String(someNumber)
+//-------------------------------------------------------------------------------------//
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hellow"
+let str2 = "abdullah"
+ 
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
